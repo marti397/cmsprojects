6da6565f5fc554b5c4861f51ea4843dd2f35c466
chrm11 <chrm11@Christophers-MacBook-Pro.local>
2017-12-18 12:55:20 -0600

    changes

--- web/collare_store/index.php
+++ web/collare_store/index.php
@@ -3,7 +3,7 @@
 	<head>
 		<title>COLLARE</title>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" href="items.css" type="text/css" />
+		<link rel="stylesheet" href="items.css" type="text/css"/>
         <!--Connection to JQuery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
 	</head>
@@ -12,7 +12,14 @@
 		<div id="mainPanel">
 			<a href="items.php"><p id="mainText">LOOK<br/>MORE<br/>ITEMS</p></a>
 			<img src="indexPics/girl1.jpg" alt="Girl 1" class="picMain">
-			<img src="indexPics/img1.jpg" alt="Img 1" class="picMain">
+			<div class="container">
+				<img src="indexPics/img1.jpg" alt="Img 1" class="picMain">
+				<div class="center">
+					<a href="items.php">
+						<p id="mainText">SEE<br>ALL<br>ITEMS</p>
+					</a>
+				</div>
+			</div>
 			<img src="indexPics/girl2.jpg" alt="Girl 2" class="picMain">
 		</div>
 		<?php include("footer.php"); ?>
